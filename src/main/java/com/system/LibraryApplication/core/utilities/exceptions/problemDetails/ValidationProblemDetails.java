9793a890b98c995061c6ff76ca3e0434ba0fc83f
fatih <kilincfatih44@gmail.com>
2024-05-09 21:00:55 +0300

    Global Exception

---
+++ src/main/java/com/system/LibraryApplication/core/utilities/exceptions/problemDetails/ValidationProblemDetails.java
@@ -0,0 +1,15 @@
+package com.system.LibraryApplication.core.utilities.exceptions.problemDetails;
+
+import lombok.Data;
+import java.util.Map;
+
+@Data
+public class ValidationProblemDetails extends ProblemDetails{
+    public ValidationProblemDetails(){
+        setTitle("Validation Rule Violation");
+        setDetail("Validation Problem");
+        setType("http://mydomain.com/exceptions/validation");
+        setStatus("400");
+    }
+    private Map<String,String> errors;
+}
